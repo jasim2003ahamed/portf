b09461e671bedf19afc64ae5bea7601adf0116bd
jasimahamed <jasim2003ahamed@gmail.com>
2025-04-14 06:21:34 +0530

    changed

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -24,8 +24,9 @@ function Projects() {
           },
           {
             image:EcomImg,
-            description:'Hello',
-            link:''
+            description:'A simple and responsive eCommerce web application built using React.js and Tailwind CSS. Users can browse products (like phones and laptops), add them to a shopping cart',
+            link:'https://jasim2003ahamed.github.io/E-comm/',
+            code:'https://github.com/jasim2003ahamed/E-comm'
           },
         ]
       }
@@ -39,11 +40,11 @@ function Projects() {
             <p className='text-zinc-600 font-semibold'>These are some of my best Projects, I have built these with React and MERN, <span className='bg-four'>Check them Out.</span></p>
         </div>
 
-        <div className='w-full px-5 py-12   '>
+        <div className='w-full px-5  py-12   '>
             <div className='gap-7 flex flex-col md:flex-row  px-10'>
 
               {config.projects.map((project) => (
-                <div className='relative shadow-lg'>
+                <div className='relative shadow-lg '>
                     <img src={project.image} className='h-[200px] w-[500px] rounded-md ' />
                   <div className='project-desc'>
                     <p className='text-center text-white py-5'>{project.description}</p>
